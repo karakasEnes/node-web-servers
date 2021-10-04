d59f5543ab4294b71d4e4218922ec1028c6045f6
pyuser <enes.karakas.e@gmail.com>
2021-10-04 12:46:18 +0300

    trying to fix

--- src/app.js
+++ src/app.js
@@ -3,8 +3,8 @@ const express = require("express");
 const hbs = require("hbs");
 const forecast = require("./utils/forecast");
 const geocode = require("./utils/geocode");
-const app = express();
 const port = process.env.PORT || 3000;
+const app = express();
 
 // Define paths
 const publicDirectoryPath = path.join(__dirname, "../public");
